d7dcd9729dda28da6969262a6dc68fa8ed35288f
Jan Miszczyk <jachuuu_15@hotmail.com>
2020-03-19 16:20:00 +0100

    Added UserDto

---
+++ YellowNotes.Core/Dtos/UserDto.cs
@@ -0,0 +1,11 @@
+namespace YellowNotes.Core.Dtos
+{
+    public class UserDto
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string NewPassword { get; set; }
+    }
+}
