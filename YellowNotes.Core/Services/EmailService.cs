9d4ed0965e9ffa8feb6e0a20aec10edc004d92e3
jaqb8 <qba.anisz@gmail.com>
2020-03-21 12:46:35 +0100

    email service

---
+++ YellowNotes.Core/Services/EmailService.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace YellowNotes.Core.Services
+{
+    public class EmailService : IEmailService
+    {
+        public async Task SendEmail(string fromEmailAddress, string toEmailAddress, string subject, string message)
+        {
+            var email = new MimeMessage();
+            email.From.Add(new MailboxAddress(fromEmailAddress));
+            email.To.Add(new MailboxAddress(toEmailAddress));
+            email.Subject = subject;
+
+            var body = new BodyBuilder
+            {
+                HtmlBody = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.ServerCertificateValidationCallback = (sender, certificate, certChainType, errors) => true;
+                await client.ConnectAsync("smpt.host", 000, false).ConfigureAwait(false);
+                await client.AuthenticateAsync("username", "password").ConfigureAwait(false);
+
+                await client.SendAsync(email).ConfigureAwait(false);
+                await client.DisconnectAsync(true).ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ YellowNotes.Core/Services/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace YellowNotes.Core.Services
+{
+    public interface IEmailService
+    {
+        Task SendEmail(
+                string fromEmailAddress,
+                string toEmailAddress,
+                string subject,
+                string message
+            );
+    }
+}
