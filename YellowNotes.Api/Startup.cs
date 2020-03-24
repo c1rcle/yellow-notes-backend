5ee877e908de3882db901cabe16d7398fa7d2366
Mateusz Nawrat <mateusz.nawrat@wp.pl>
2020-03-24 10:19:18 +0100

    Removed unused imports

--- YellowNotes.Api/Startup.cs
+++ YellowNotes.Api/Startup.cs
@@ -6,8 +6,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using YellowNotes.Core;
 using YellowNotes.Core.Email;
 using YellowNotes.Core.Services;
