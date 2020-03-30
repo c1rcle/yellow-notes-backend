3e54f48d17e62d80b4b1bb0a97a02421daa14b08
Jan Miszczyk <jachuuu_15@hotmail.com>
2020-03-30 14:18:49 +0200

    Added missing files

---
+++ YellowNotes.Core/Migrations/20200330121358_CreateNoteEntity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YellowNotes.Core.Migrations
+{
+    public partial class CreateNoteEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    NoteId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserEmail = table.Column<string>(unicode: false, nullable: false),
+                    ModificationDate = table.Column<DateTime>(nullable: false),
+                    Variant = table.Column<string>(unicode: false, nullable: false),
+                    IsRemoved = table.Column<bool>(nullable: false),
+                    Content = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.NoteId);
+                    table.ForeignKey(
+                        name: "FK_User",
+                        column: x => x.UserEmail,
+                        principalTable: "Users",
+                        principalColumn: "Email",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_UserEmail",
+                table: "Notes",
+                column: "UserEmail");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
